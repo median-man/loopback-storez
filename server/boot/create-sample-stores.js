1055554aae80be31fa562f267286726760df6773
John Desrosiers <john.desro@gmail.com>
2018-10-31 06:37:57 -0700

    Add sample store model instances on startup.
    Add 3 sample instances of the store model when the server boots.

---
+++ server/boot/create-sample-stores.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = function createSampleStores(app) {
+  app.dataSources.mongoDS.automigrate(
+    'store',
+    err => err ? createStoresErr(err) : createStores(app)
+  );
+};
+
+function createStoresErr(err) {
+  throw err;
+}
+
+function createStores(app) {
+  const sampleStores = [{
+    name: 'Fireside Brews',
+    city: 'Mammoth Lakes',
+  }, {
+    name: 'Cereal Farm',
+    city: 'El Cajon',
+  }, {
+    name: 'Pies Galore',
+    city: 'Lake Tahoe',
+  }];
+  app.models.store.create(sampleStores)
+    .then(logStores)
+    .catch(console.error);
+}
+
+function logStores(stores) {
+  console.log('Models created: \n', stores);
+}
