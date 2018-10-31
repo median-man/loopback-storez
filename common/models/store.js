c1808042798cf010bc5297c8efdbc2465ebc1dfc
John Desrosiers <john.desro@gmail.com>
2018-10-31 08:06:30 -0700

    Create /stores/getStatus route Create /stores/getStatus api route which returns a message indicating if stores are open or closed.

--- common/models/store.js
+++ common/models/store.js
@@ -1,5 +1,33 @@
 'use strict';
 
-module.exports = function(Store) {
-
+module.exports = function defineStoreRemoteMethods(Store) {
+  const methodName = 'status';
+  const options = {
+    http: {
+      path: '/status',
+      verb: 'get',
+    },
+    returns: {
+      arg: 'status',
+      type: 'string',
+    },
+  };
+  Store[methodName] = statusMessage;
+  Store.remoteMethod(methodName, options);
 };
+
+function statusMessage() {
+  const OPEN_MESSAGE = 'We are open for business.';
+  const CLOSED_MESSAGE = 'Sorry, we are closed. Open daily from 6am to 8pm.';
+  const response = isStoreOpen() ? OPEN_MESSAGE : CLOSED_MESSAGE;
+  return Promise.resolve(response);
+}
+
+function isStoreOpen() {
+  const currentHour = new Date().getHours();
+  console.log(`Current hour is ${currentHour}`);
+
+  const OPEN_HOUR = 6;
+  const CLOSE_HOUR = 20;
+  return currentHour >= OPEN_HOUR && currentHour < CLOSE_HOUR;
+}
